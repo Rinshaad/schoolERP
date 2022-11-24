ce8e1f7f00a0a8a0421af6b3194642773e7eac92
Rinshaad <rinshadm007@gmail.com>
2022-11-24 13:04:34 +0530

    domain changed

--- schoolERP/settings.py
+++ schoolERP/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-cd&lvct^5dmjqx4p62*%mo90fv&%%jg9zev%7(4d9n#b7&h*6v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['baabtraproject1.herokuapp.com']
+ALLOWED_HOSTS = ['baabtraproject1.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
